c3ef5cb66a94de7c3e4496e5533fb47b9f31af62
Dim <me@dimps.name>
2022-01-17 23:33:26 -0500

    Testing public

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static("public"))
+
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello World')
 })
 
 const port = process.env.PORT || 3000
